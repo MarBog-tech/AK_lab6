3fa84353c54101cce4d74ba7c3e3f5ee8b0e856e
MarBog-tech <marchuk_bogdan@i.ua>
2022-12-16 22:54:23 +0200

    lab6

---
+++ .hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/lenovo/AK/AK_lab6/test2/hello.ko := ccache arm-linux-gnueabihf-ld -r -EL -z noexecstack -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/lenovo/AK/AK_lab6/test2/hello.ko /home/lenovo/AK/AK_lab6/test2/hello.o /home/lenovo/AK/AK_lab6/test2/hello.mod.o ;  true
---
+++ .tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/lenovo/AK/AK_lab6/test2/hello.ko
+/home/lenovo/AK/AK_lab6/test2/hello.o
+
